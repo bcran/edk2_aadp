e36cac831710578bca804306f37b47aec0f8f20d
Phil.Xing <phil.xing@adlinktech.com>
2022-05-12 15:49:29 +0800

    Fix make_jade error and revert edk2 modifications.

--- make_jade.sh
+++ make_jade.sh
@@ -4,7 +4,6 @@ OPATH=$PATH
 make -f $WORKSPACE/edk2-ampere-tools/Makefile \
     BOARD_SETTING=$WORKSPACE/AmpereAltra-ATF-SCP/board_settings/jade_board_setting_2.06.20220308.bin \
     VM_SHARED_DIR=$HOME/AmpereR \
-    PACKAGES_PATH=$WORKSPACE/adlink-platforms:"${PACKAGES_PATH}" \
     ATF_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/atf/altra_atf_signed_2.06.20220308.slim \
     SCP_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/scp/altra_scp_signed_2.06.20220308.slim \
     LINUXBOOT_BIN=$WORKSPACE/flashkernel \
