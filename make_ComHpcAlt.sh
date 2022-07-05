36525059142d233f5fd128223a1402aa8a22e7ad
Phil Xing <phil.xing@adlinktech.com>
2022-07-05 16:04:40 +0800

    Update SCP FW to 2.10.

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -4,8 +4,8 @@ DEVELMENT_MODE=0
 #
 # Firmware Version
 #
-VER=2.06
-BUILD=100.00
-DEBUG=0
+# VER=2.06
+# BUILD=100.01
+# DEBUG=0
 
 . edk2_adlink-ampere-altra/tools/make_adlink.sh ComHpcAlt A2
--- make_jade.sh
+++ make_jade.sh
@@ -3,12 +3,12 @@ export WORKSPACE=$PWD
 OPATH=$PATH
 . $WORKSPACE/edk2_adlink-ampere-altra/tools/edk2.sh
 make -f $WORKSPACE/edk2-ampere-tools/Makefile \
-    BOARD_SETTING=$WORKSPACE/AmpereAltra-ATF-SCP/board_settings/jade_board_setting_2.06.20220308.bin \
+    BOARD_SETTING=$WORKSPACE/AmpereAltra-ATF-SCP/board_settings/jade_board_setting_2.10.20220531.bin \
     VM_SHARED_DIR=$HOME/AmpereR \
     PACKAGES_PATH=$WORKSPACE/adlink-platforms:"${PACKAGES_PATH}" \
-    ATF_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/atf/altra_atf_signed_2.06.20220308.slim \
-    SCP_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/scp/altra_scp_signed_2.06.20220308.slim \
+    ATF_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/atf/altra_atf_signed_2.10.20220531.slim \
+    SCP_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/scp/altra_scp_signed_2.10.20220531.slim \
     LINUXBOOT_BIN=$WORKSPACE/flashkernel \
-    VER=2.05 BUILD=100 \
+    VER=2.06 BUILD=100 \
     tianocore_capsule # tianocore_img # linuxboot_img # all # 
 export PATH=$OPATH
