be2865a336f25a192f5dfdb50e17ccefe1fa0df4
Phil.Xing <phil.xing@adlinktech.com>
2022-10-06 16:44:43 +0800

    Update scripts.

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -6,8 +6,7 @@ DEVELMENT_MODE=0
 #
 VER=2.04
 BUILD=100.07
+ATF_SCP_VER=2.06.20220308
 DEBUG=0
-ATF_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/atf/altra_atf_signed_2.06.20220308.slim
-SCP_SLIM=$WORKSPACE/AmpereAltra-ATF-SCP/scp/altra_scp_signed_2.06.20220308.slim
 
 . edk2_adlink-ampere-altra/tools/make_adlink.sh ComHpcAlt A2
